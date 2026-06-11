d8c0f08d7811559fcda0c0edbe87007caea2252b
Marcos Javier Magni Mattoni <javier.magni@gmail.com>
2026-06-11 15:18:39 +0200

    feat: redirect /exploracion to booking URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   adapter: netlify(),
+  redirects: {
+    '/exploracion': 'https://outlook.office.com/bookwithme/user/48d235d03a08496b97d4b477ea665fbb@virtusway.com/meetingtype/l9E3R2UjHUaYl4B09FL6MQ2?anonymous&ep=mlink',
+  },
 });
